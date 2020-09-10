107e3823cdf90a031f3c210bb47ac5c13ee87434
Maurits van Rees <maurits@vanrees.org>
2020-09-10 11:43:25 +0200

    Initial code structure.
    Lightly based on bobtemplates.plone addon.

---
+++ README.rst
@@ -0,0 +1,73 @@
+.. This README is meant for consumption by humans and pypi. Pypi can render rst files so please do not use Sphinx features.
+   If you want to learn more about writing documentation, please check out: http://docs.plone.org/about/documentation_styleguide.html
+   This text does not appear on pypi or github. It is a comment.
+
+.. image:: https://travis-ci.org/plone/plone.autoinclude.svg?branch=master
+    :target: https://travis-ci.org/plone/plone.autoinclude
+
+.. image:: https://coveralls.io/repos/github/plone/plone.autoinclude/badge.svg?branch=master
+    :target: https://coveralls.io/github/plone/plone.autoinclude?branch=master
+    :alt: Coveralls
+
+.. image:: https://img.shields.io/pypi/v/plone.autoinclude.svg
+    :target: https://pypi.python.org/pypi/plone.autoinclude/
+    :alt: Latest Version
+
+.. image:: https://img.shields.io/pypi/status/plone.autoinclude.svg
+    :target: https://pypi.python.org/pypi/plone.autoinclude
+    :alt: Egg Status
+
+.. image:: https://img.shields.io/pypi/pyversions/plone.autoinclude.svg?style=plastic   :alt: Supported - Python Versions
+
+.. image:: https://img.shields.io/pypi/l/plone.autoinclude.svg
+    :target: https://pypi.python.org/pypi/plone.autoinclude/
+    :alt: License
+
+
+=================
+plone.autoinclude
+=================
+
+Tell me what your product does
+
+Features
+--------
+
+- Can be bullet points
+
+
+Installation
+------------
+
+Install plone.autoinclude by adding it to your buildout::
+
+    [buildout]
+
+    ...
+
+    eggs =
+        plone.autoinclude
+    zcml =
+        plone.autoinclude
+
+
+and then running ``bin/buildout``
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/plone/plone.autoinclude/issues
+- Source Code: https://github.com/plone/plone.autoinclude
+
+
+Support
+-------
+
+If you are having issues, please let us know.
+
+
+License
+-------
+
+The project is licensed under the GPLv2.
---
+++ setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Installer for the plone.autoinclude package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='plone.autoinclude',
+    version='1.0.0a1',
+    description="Auto include code and zcml",
+    # long_description: see metadata in setup.cfg
+    # Get more from https://pypi.org/classifiers/
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='Python Plone CMS',
+    author='Maurits van Rees',
+    author_email='maurits@vanrees.org',
+    url='https://github.com/collective/plone.autoinclude',
+    project_urls={
+        'PyPI': 'https://pypi.org/project/plone.autoinclude/',
+        'Source': 'https://github.com/plone/plone.autoinclude',
+        'Tracker': 'https://github.com/plone/plone.autoinclude/issues',
+        # 'Documentation': 'https://plone.autoinclude.readthedocs.io/en/latest/',
+    },
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires="==2.7, >=3.6",
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={
+        # 'test': [
+        #     # Maybe
+        #     'plone.testing>=5.0.0',
+        # ],
+    },
+    # Just as a reminder of a z3c.autoinclude entry point:
+    # entry_points="""
+    # [z3c.autoinclude.plugin]
+    # target = plone
+    # """,
+)
