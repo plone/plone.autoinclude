12cc1f9c6f1036bf0a8d8c96b5ae63697be1bb47
Maurits van Rees <maurits@vanrees.org>
2021-02-10 23:30:02 +0100

    Adapt tests now that example.ploneaddon is installed.

--- src/plone/autoinclude/tests/test_loader.py
+++ src/plone/autoinclude/tests/test_loader.py
@@ -5,13 +5,23 @@ class TestLoader(unittest.TestCase):
     def test_load_packages(self):
         from plone.autoinclude.loader import load_packages
 
-        self.assertEqual(load_packages(), {})
+        packages = load_packages()
+        self.assertEqual(sorted(packages.keys()), ["example.ploneaddon"])
+        package = packages["example.ploneaddon"]
+        import example.ploneaddon
+
+        self.assertEqual(package, example.ploneaddon)
 
     def test_get_zcml_file(self):
         from plone.autoinclude.loader import get_zcml_file
 
         self.assertIsNone(get_zcml_file("zope.configuration"))
         self.assertIsNone(get_zcml_file("zope.configuration", zcml="foo.zcml"))
+        filename = get_zcml_file("example.ploneaddon")
+        self.assertIsNotNone(filename)
+        with open(filename) as myfile:
+            self.assertIn("This is configure.zcml from example.ploneaddon.", myfile.read())
+        self.assertIsNone(get_zcml_file("example.ploneaddon", zcml="foo.zcml"))
 
     def test_load_zcml_file(self):
         from plone.autoinclude.loader import load_zcml_file
