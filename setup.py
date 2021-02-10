622b0784b7c0bb5bdc50fee54ba978c99c8482b5
Maurits van Rees <maurits@vanrees.org>
2021-02-10 22:04:17 +0100

    Configuring for pure-python
    Adapted from https://github.com/zopefoundation/meta/tree/master/config

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -39,7 +40,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="~=3.6",
+    python_requires=">=3.6",
     install_requires=[
         "setuptools",
     ],
