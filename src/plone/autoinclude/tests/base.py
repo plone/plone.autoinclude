b240232789f95b5d813db2dd97416d04c81b20ef
Maurits van Rees <maurits@vanrees.org>
2021-02-11 13:52:10 +0100

    Create tests.base.PackageTestCase.
    Other package test cases should inherit from unittest.TestCase plus this base class.
    
    The base class itself intentionally does not inherit unittest.TestCase,
    otherwise the test runner tries to run the tests from the base class as well.
    Solution found at https://intellipaat.com/community/68718/python-unit-test-with-base-and-sub-class?show=68719

---
+++ src/plone/autoinclude/tests/base.py
@@ -0,0 +1,99 @@
+from .utils import get_configuration_context
+from importlib import import_module
+
+
+class PackageTestCase:
+    """Test our interaction with a package.
+
+    This is a base test class for all packages.
+    You can inherit from this class and override the variables.
+
+    The base class intentionally does not inherit unittest.TestCase,
+    otherwise the test runner tries to run the tests from the base class as well.
+    """
+
+    project_name = ""
+    # How many files are loaded when we load meta.zcml, configure.zcml, overrides.zcml?
+    meta_files = 0
+    configure_files = 3
+    overrides_files = 0
+
+    def test_load_packages(self):
+        from plone.autoinclude.loader import load_packages
+
+        packages = load_packages()
+        self.assertIn(self.project_name, packages.keys())
+        loaded_package = packages[self.project_name]
+        imported_package = import_module(self.project_name)
+        self.assertEqual(loaded_package, imported_package)
+
+    def test_get_zcml_file_non_existing(self):
+        from plone.autoinclude.loader import get_zcml_file
+
+        self.assertIsNone(get_zcml_file(self.project_name, zcml="foo.zcml"))
+
+    def test_get_zcml_file_default(self):
+        from plone.autoinclude.loader import get_zcml_file
+
+        filename = get_zcml_file(self.project_name)
+        if not self.configure_files:
+            self.assertIsNone(filename)
+            return
+        self.assertIsNotNone(filename)
+        with open(filename) as myfile:
+            self.assertIn(
+                f"This is configure.zcml from {self.project_name}.", myfile.read()
+            )
+
+    def _context(self, package=None):
+        # Various functions take a configuration context as argument.
+        # From looking at zope.configuration.xmlconfig.file the following seems about right.
+        from zope.configuration.config import ConfigurationMachine
+        from zope.configuration.xmlconfig import registerCommonDirectives
+
+        context = ConfigurationMachine()
+        registerCommonDirectives(context)
+        if package is not None:
+            # When you set context.package, context.path(filename) works nicely.
+            context.package = package
+        return context
+
+    def test_load_zcml_file_configure(self):
+        from plone.autoinclude.loader import load_zcml_file
+
+        # prepare configuration context
+        package = import_module(self.project_name)
+        context = get_configuration_context(package)
+        self.assertEqual(len(context._seen_files), 0)
+
+        # Load configure.zcml.
+        load_zcml_file(context, self.project_name, package)
+        self.assertEqual(len(context._seen_files), self.configure_files)
+        self.assertIn(context.path("configure.zcml"), context._seen_files)
+        # This includes two other files.
+        self.assertIn(context.path("permissions.zcml"), context._seen_files)
+        self.assertIn(context.path("browser/configure.zcml"), context._seen_files)
+
+    def test_load_zcml_file_non_existing(self):
+        from plone.autoinclude.loader import load_zcml_file
+
+        package = import_module(self.project_name)
+        context = get_configuration_context(package)
+        load_zcml_file(context, self.project_name, package, zcml="foo.zcml")
+        self.assertEqual(len(context._seen_files), 0)
+
+    def test_load_zcml_file_overrides(self):
+        from plone.autoinclude.loader import load_zcml_file
+
+        package = import_module(self.project_name)
+        context = get_configuration_context(package)
+        # currently we have no override
+        load_zcml_file(
+            context, self.project_name, package, "overrides.zcml", override=True
+        )
+        self.assertEqual(len(context._seen_files), self.overrides_files)
+
+
+# def suite():
+#     suite = unittest.TestSuite()
+#     return suite
--- src/plone/autoinclude/tests/test_package_example_ploneaddon.py
+++ src/plone/autoinclude/tests/test_package_example_ploneaddon.py
@@ -1,15 +1,12 @@
-from .utils import get_configuration_context
-from importlib import import_module
+from .base import PackageTestCase
 
 import unittest
 
 
-class TestPackagePloneAddon(unittest.TestCase):
+class TestPackagePloneAddon(unittest.TestCase, PackageTestCase):
     """Test our interaction with the example.ploneaddon package.
 
     You can find this package in the test-packages directory in the repo.
-    We may want to create a base test class for all packages.
-    But we start with one.
     """
 
     project_name = "example.ploneaddon"
@@ -17,78 +14,3 @@ class TestPackagePloneAddon(unittest.TestCase):
     meta_files = 0
     configure_files = 3
     overrides_files = 0
-
-    def test_load_packages(self):
-        from plone.autoinclude.loader import load_packages
-
-        packages = load_packages()
-        self.assertIn(self.project_name, packages.keys())
-        loaded_package = packages[self.project_name]
-        imported_package = import_module(self.project_name)
-        self.assertEqual(loaded_package, imported_package)
-
-    def test_get_zcml_file_non_existing(self):
-        from plone.autoinclude.loader import get_zcml_file
-
-        self.assertIsNone(get_zcml_file(self.project_name, zcml="foo.zcml"))
-
-    def test_get_zcml_file_default(self):
-        from plone.autoinclude.loader import get_zcml_file
-
-        filename = get_zcml_file(self.project_name)
-        if not self.configure_files:
-            self.assertIsNone(filename)
-            return
-        self.assertIsNotNone(filename)
-        with open(filename) as myfile:
-            self.assertIn(
-                f"This is configure.zcml from {self.project_name}.", myfile.read()
-            )
-
-    def _context(self, package=None):
-        # Various functions take a configuration context as argument.
-        # From looking at zope.configuration.xmlconfig.file the following seems about right.
-        from zope.configuration.config import ConfigurationMachine
-        from zope.configuration.xmlconfig import registerCommonDirectives
-
-        context = ConfigurationMachine()
-        registerCommonDirectives(context)
-        if package is not None:
-            # When you set context.package, context.path(filename) works nicely.
-            context.package = package
-        return context
-
-    def test_load_zcml_file_configure(self):
-        from plone.autoinclude.loader import load_zcml_file
-
-        # prepare configuration context
-        package = import_module(self.project_name)
-        context = get_configuration_context(package)
-        self.assertEqual(len(context._seen_files), 0)
-
-        # Load configure.zcml.
-        load_zcml_file(context, self.project_name, package)
-        self.assertEqual(len(context._seen_files), self.configure_files)
-        self.assertIn(context.path("configure.zcml"), context._seen_files)
-        # This includes two other files.
-        self.assertIn(context.path("permissions.zcml"), context._seen_files)
-        self.assertIn(context.path("browser/configure.zcml"), context._seen_files)
-
-    def test_load_zcml_file_non_existing(self):
-        from plone.autoinclude.loader import load_zcml_file
-
-        package = import_module(self.project_name)
-        context = get_configuration_context(package)
-        load_zcml_file(context, self.project_name, package, zcml="foo.zcml")
-        self.assertEqual(len(context._seen_files), 0)
-
-    def test_load_zcml_file_overrides(self):
-        from plone.autoinclude.loader import load_zcml_file
-
-        package = import_module(self.project_name)
-        context = get_configuration_context(package)
-        # currently we have no override
-        load_zcml_file(
-            context, self.project_name, package, "overrides.zcml", override=True
-        )
-        self.assertEqual(len(context._seen_files), self.overrides_files)
