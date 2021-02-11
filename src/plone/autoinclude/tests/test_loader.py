26083d905e8fa71d3dd45ace1aa879c6fc18cc43
Thomas Schorr <t_schorr@gmx.de>
2021-02-11 17:50:37 +0100

    remove added working_set entries on tear down; add setup/tearDown to other tests

--- src/plone/autoinclude/tests/test_loader.py
+++ src/plone/autoinclude/tests/test_loader.py
@@ -17,6 +17,7 @@ class TestLoader(unittest.TestCase):
         self.stored_syspath = copy(sys.path)
         projects_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'test-packages')  # noqa: E501
         test_packages = os.listdir(projects_dir)
+        self.added_entries = []
         for package in test_packages:
             packagedir = os.path.join(projects_dir, package)
             os.chdir(packagedir)
@@ -30,12 +31,16 @@ class TestLoader(unittest.TestCase):
             ei.run()
             egginfodir = os.path.join(packagedir, 'src')
             workingset.add_entry(egginfodir)
+            self.added_entries.append(egginfodir)
             sys.path.append(egginfodir)
         os.chdir(self.workingdir)
 
     def tearDown(self):
         os.chdir(self.workingdir)
         sys.path = self.stored_syspath
+        workingset = pkg_resources.working_set
+        for entry in self.added_entries:
+            workingset.entries.remove(entry)
 
     def test_load_packages(self):
         from plone.autoinclude.loader import load_packages
--- src/plone/autoinclude/tests/test_package_example_metaoverrides.py
+++ src/plone/autoinclude/tests/test_package_example_metaoverrides.py
@@ -1,3 +1,9 @@
+from copy import copy
+import os
+import sys
+import pkg_resources
+from setuptools.command.egg_info import egg_info
+import distutils.core
 from .base import PackageTestCase
 
 import unittest
@@ -8,3 +14,34 @@ class TestPackage(unittest.TestCase, PackageTestCase):
     # This package only has meta.zcml and overrides.zcml, no configure.zcml.
     configure_files = []
     features = ["metaoverrides"]
+
+    def setUp(self):
+        workingset = pkg_resources.working_set
+        self.workingdir = os.getcwd()
+        self.stored_syspath = copy(sys.path)
+        projects_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'test-packages')  # noqa: E501
+        test_packages = os.listdir(projects_dir)
+        self.added_entries = []
+        for package in test_packages:
+            packagedir = os.path.join(projects_dir, package)
+            os.chdir(packagedir)
+            dist = distutils.core.run_setup('setup.py')
+            ei = egg_info(dist)
+            ei.finalize_options()
+            try:
+                os.mkdir(ei.egg_info)
+            except FileExistsError:
+                pass
+            ei.run()
+            egginfodir = os.path.join(packagedir, 'src')
+            workingset.add_entry(egginfodir)
+            self.added_entries.append(egginfodir)
+            sys.path.append(egginfodir)
+        os.chdir(self.workingdir)
+
+    def tearDown(self):
+        os.chdir(self.workingdir)
+        sys.path = self.stored_syspath
+        workingset = pkg_resources.working_set
+        for entry in self.added_entries:
+            workingset.entries.remove(entry)
