692c46b16859129373cb977351b1bb38477d6189
Thomas Schorr <t_schorr@gmx.de>
2021-02-11 14:54:27 +0100

    setup test packages locally

--- src/plone/autoinclude/tests/test_loader.py
+++ src/plone/autoinclude/tests/test_loader.py
@@ -1,12 +1,46 @@
+from copy import copy
+import os
+import sys
+import pkg_resources
+from setuptools.command.egg_info import egg_info
+import distutils.core
+
 import unittest
 
 
 class TestLoader(unittest.TestCase):
+
+    def setUp(self):
+        workingset = pkg_resources.working_set
+        self.workingdir = os.getcwd()
+        self.stored_syspath = copy(sys.path)
+        projects_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'test-packages')  # noqa: E501
+        test_packages = os.listdir(projects_dir)
+        for package in test_packages:
+            packagedir = os.path.join(projects_dir, package)
+            os.chdir(packagedir)
+            dist = distutils.core.run_setup('setup.py')
+            ei = egg_info(dist)
+            ei.finalize_options()
+            try:
+                os.mkdir(ei.egg_info)
+            except FileExistsError:
+                pass
+            ei.run()
+            egginfodir = os.path.join(packagedir, 'src')
+            workingset.add_entry(egginfodir)
+            sys.path.append(egginfodir)
+        os.chdir(self.workingdir)
+
+    def tearDown(self):
+        os.chdir(self.workingdir)
+        sys.path = self.stored_syspath
+
     def test_load_packages(self):
         from plone.autoinclude.loader import load_packages
 
         packages = load_packages()
-        self.assertEqual(sorted(packages.keys()), ["example.ploneaddon"])
+        self.assertIn('example.ploneaddon', packages.keys())
         package = packages["example.ploneaddon"]
         import example.ploneaddon
 
