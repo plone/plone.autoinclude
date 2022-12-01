e403f06085004825bba6f6f1c44abf46dcb91101
Maurits van Rees <maurits@vanrees.org>
2022-12-01 23:34:16 +0100

    Back to development: 1.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="plone.autoinclude",
-    version="1.0.0",
+    version="1.0.1.dev0",
     description="Auto include code and zcml",
     # long_description: see metadata in setup.cfg
     # Get more from https://pypi.org/classifiers/
